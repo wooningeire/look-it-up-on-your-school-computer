eb5d782d0543d2a8c08b179677925aad2fc7e1dd
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-01-26 21:11:20 -0500

    feat: add logging to zybooks speedrunner

--- zybooks_speedrunner.js
+++ zybooks_speedrunner.js
@@ -1,11 +1,13 @@
 // multiple choice
-[...document.querySelectorAll(".multiple-choice-question, .definition-match-question")].forEach(async q => {
-    for (const x of q.querySelectorAll("input[type='radio']")) {
-		x.click();
-		await new Promise(resolve => setTimeout(resolve, 250));
-		if (q.querySelector(".correct")) break;
-    }
-});
+Promise.all(
+    [...document.querySelectorAll(".multiple-choice-question, .definition-match-question")].map(async q => {
+        for (const x of q.querySelectorAll("input[type='radio']")) {
+            x.click();
+            await new Promise(resolve => setTimeout(resolve, 250));
+            if (q.querySelector(".correct")) break;
+        }
+    })
+).then(() => console.log("multiple choices done"));
 // to answer drag and drop questions, enable accessibility mode in the book subscription settings
 
 // short answer
@@ -18,18 +20,28 @@ setTimeout(() => {
         t.dispatchEvent(new KeyboardEvent("input"));
         q.querySelector(".check-button").click();
     });
+    console.log("short answers done");
 }, 250);
 
 // animation
-[...document.querySelectorAll(".animation-controls")].forEach(async c => {
-    c.querySelector(".start-button")?.click();
-    c.querySelector("input[type='checkbox']").click();
+Promise.all(
+    [...document.querySelectorAll(".animation-controls")].map(async c => {
+        c.querySelector(".start-button")?.click();
+        c.querySelector("input[type='checkbox']").click();
 
-    while (!c.querySelector("[aria-label='Play again']")) {
-        await new Promise(resolve => setTimeout(resolve, 250));
-        c.querySelector("[aria-label='Play']")?.click();    
-    }
-});
+        while (!c.querySelector("[aria-label='Play again']")) {
+            await new Promise(resolve => setTimeout(resolve, 250));
+            c.querySelector("[aria-label='Play']")?.click();    
+        }
+    })
+).then(() => console.log("animations done"));
 
 // simulation
 [...document.querySelectorAll(".enter-simulation")].forEach(x => x.click());
+console.log("simulations done");
+
+// custom tools
+[...document.querySelectorAll(".content-tool-container")].forEach(c => {
+    c.querySelector("button").click();
+});
+console.log("custom tools done");
