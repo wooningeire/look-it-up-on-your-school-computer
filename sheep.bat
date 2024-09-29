e561f028e6a32decff9a51976f03480b75428d23
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-09-28 21:07:06 -0400

    feat: sheep.bat

---
+++ sheep.bat
@@ -0,0 +1,21 @@
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -inprivate "https://www.youtube.com/watch?v=gxT8mxiu6Hw" --autoplay-policy=no-user-gesture-required
+(
+	echo #Fri Sep 06 22:15:19 EDT 2024
+	echo debug=false
+	echo disable-large-downloads=false
+	echo incompatible-process=
+	echo auto-signin=true
+	echo priority=19
+	echo login=szedelacity
+	echo headless=false
+	echo proxy=
+	echo password=
+	echo hostname=
+	echo cores=32
+	echo ui=swing
+	echo use-systray=false
+	echo compute-method=CPU
+	echo theme=light
+	echo ram=32000000k
+) > "C:\Users\~\.sheepit.conf"
+"V:\Desktop\sheepit-autoupdate.exe"
